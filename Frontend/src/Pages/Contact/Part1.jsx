aae6f4589736cbfd0b5d7829cce8011a1fb34716
sharma8979 <anantsharma89790@gmail.com>
2025-01-02 02:00:46 +0530

    Contact Us done

--- Frontend/src/Pages/Contact/Part1.jsx
+++ Frontend/src/Pages/Contact/Part1.jsx
@@ -2,13 +2,29 @@ import React from 'react'
 
 const Part1 = () => {
   return (
-    <div className='min-h-screen flex'>
-        <div className='p-[10vw] w-[50%] bg-red-200'>
-           <img className='h-[0vw]  bg-cover'
-           src="https://plus.unsplash.com/premium_photo-1667762241847-37471e8c8bc0?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aGVhbHRofGVufDB8fDB8fHww" alt="" />
-        </div>
-        <div className='bg-red-300 w-[50%]'>
+    <div className='  flex'>
+
+        <div className='p-[10vw]  w-[50%] '>
+            <div className='bg-blue-200 h-[70vh] w-[35vw] bg-cover rounded-lg
+            bg-[url(https://plus.unsplash.com/premium_photo-1667762241847-37471e8c8bc0?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aGVhbHRofGVufDB8fDB8fHww)]
+            '>
+            </div>
+         </div>
+
+        <div className=' w-[50%]'>
+
+            <div className='mt-[23vw] w-[80%]  '>
+             <h1 className='mb-5 text-gray-600'>Beautiful Home</h1>   
             <h1 className=' text-3xl'>Made With Love</h1>
+            <h1 className='text-xl text-gray-500 font-light mt-5 '>
+            Our candles are made with natural scents and 100% soy wax. These beautiful additions to your
+             cozy home are made with care and craft. If you
+             have any questions or special requests, feel free to get in touch with us. 
+            </h1>
+            <button className='mt-5 text-2xl text-center rounded-lg bg-cream w-[10vw] h-[4vw]'>
+                Explore
+            </button>
+            </div>
 
         </div>
     </div>
---
+++ Frontend/src/Pages/Contact/Part3.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Part3 = () => {
+  return (
+    <div className='bg-red-300  min-h-screen bg-cover rounded
+    bg-[url(https://images.unsplash.com/photo-1521706862577-47b053587f91?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Z3JlZW5yeXxlbnwwfHwwfHx8MA%3D%3D)]
+    '>
+        <div className='py-10'>
+        <h1 className='text-center text-5xl text-white'>Reviews</h1>
+        </div>
+
+        <div className='flex mt-5 justify-evenly '>
+            <div className='w-[20%] rounded h-[55vh] bg-white'></div>
+            <div className='w-[20%] rounded h-[55vh] bg-white'></div>
+            <div className='w-[20%] rounded h-[55vh] bg-white'></div>
+        </div>
+    </div>
+  )
+}
+
+export default Part3
